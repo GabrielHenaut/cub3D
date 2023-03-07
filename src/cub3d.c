78ecc101dc632fb5199d8b11f8614aab17bfdace
harndt <harndt@student.42sp.org.br>
2023-03-07 20:50:35 +0100

    feat: check_args function refactored, and documentation added.

--- src/cub3d.c
+++ src/cub3d.c
@@ -3,51 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ghenaut- <ghenaut-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: harndt <harndt@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 16:52:55 by ghenaut-          #+#    #+#             */
-/*   Updated: 2023/03/06 15:35:19 by ghenaut-         ###   ########.fr       */
+/*   Updated: 2023/03/07 20:30:04 by harndt           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/cub3d.h"
+#include "cub3d.h"
 
-static int	is_valid_file_extension(char *str)
+/**
+ * @brief Checks if the map file extension is valid.
+ * 
+ * @param str Address to the given file.
+ * @return t_bool If the file is valid TRUE, else FALSE.
+ */
+static t_bool	is_valid_file_extension(char *str)
 {
 	size_t	offset;
 
 	offset = ft_strlen(str) - 4;
 	if (ft_strncmp(".cub", str + offset, 5) == 0)
-		return (1);
-	return (0);
+		return (TRUE);
+	return (FALSE);
 }
 
-static int	check_error(int argc, char **argv)
+/**
+ * @brief Checks if the received args by command line are correct.
+ * 
+ * @param argc Total number of args received by command line.
+ * @param argv Args received by command line.
+ * @return t_bool If arguments are valid TRUE, else FALSE. 
+ */
+static t_bool	check_args(int argc, char **argv)
 {
-	int		printed;
 	int		fd;
 	char	*buff;
 
-	printed = 0;
+	if (argc != 2)
+		return (msg(STR_USAGE, NULL, FALSE));
+	if (!is_valid_file_extension(argv[1]))
+		return (msg(STR_INVALID_MAP, argv[1], FALSE));
 	buff = (char *)malloc(2 + 1);
 	if (buff == NULL)
 		return (1);
 	fd = open(argv[1], O_RDONLY);
-	if (argc != 2)
-		printed = ft_printf("Error\nInvalid number of arguments\n");
-	else if (!is_valid_file_extension(argv[1]))
-		printed = ft_printf("Error\nInvalid file extension\n");
-	else if (read(fd, buff, 0) < 0)
-		printed = ft_printf("Error\nInvalid map path\n");
-	else if (read(fd, buff, 2) == 0)
-		printed = ft_printf("Error\nMap empty or with not at least 1 new line\n");
-	free(buff);
-	if (printed)
+	if (read(fd, buff, 0) < 0)
 	{
-		exit(1);
-		return (1);
+		free(buff);
+		return (msg(STR_FILE_NOT_FOUND, argv[1], FALSE));
 	}
-	return (0);
+	else if (read(fd, buff, 2) == 0)
+	{
+		free(buff);
+		return (msg(STR_MAP_EMPTY, argv[1], FALSE));
+	}
+	free(buff);
+	return (TRUE);
 }
 
 // static int	rerender(t_fdf *data)
@@ -57,16 +69,25 @@ static int	check_error(int argc, char **argv)
 	// return (0);
 // }
 
+/**
+ * @brief Main function, checks args received by command line and executes the
+ * program.
+ * 
+ * @param argc Total number of args received by command line.
+ * @param argv Args received by command line.
+ * @return int On success returns 0, esle returns 1.
+ */
 int	main(int argc, char **argv)
 {
-	t_cubed     data;
+	// t_cubed	data;
 
-	if (check_error(argc, argv) == 1)
-		return (1);
-	init_game(&data, argv[1]);
+	if (check_args(argc, argv) == 1)
+		return (EXIT_FAILURE);
+	// init_game(&data, argv[1]);
 	// mlx_hook(data.win, KEYPRESS, KEYPRESSMASK, arrows_down, &data);
 	// mlx_hook(data.win, REFOCUS, (1L << 04), rerender, &data);
 	// mlx_hook(data.win, DESTROY, DESTROYMASK, clean_exit, &data);
 	// mlx_loop(data.mlx);
-	printf("%s", data.map.tex_path_east);
+	// printf("%s", data.map.tex_path_east);
+	return (EXIT_SUCCESS);
 }
