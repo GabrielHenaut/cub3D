b3a18db912f15e05ba8d71cc687366bd9d6bbe2f
harndt <harndt@student.42sp.org.br>
2023-03-08 21:01:21 +0100

    feat: include keys header with keyboard keys mapped

---
+++ includes/keys.h
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keys.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: harndt <harndt@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/08 19:43:48 by harndt            #+#    #+#             */
+/*   Updated: 2023/03/08 19:43:50 by harndt           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef KEYS_H
+# define KEYS_H
+
+# define KEY_LEFT	65361
+# define KEY_UP		65362
+# define KEY_RIGHT	65363
+# define KEY_DOWN	65364
+
+# define NUMPAD_1	65436
+# define NUMPAD_2	65433
+# define NUMPAD_3	65435
+# define NUMPAD_4	65430
+# define NUMPAD_5	65437
+# define NUMPAD_6	65432
+# define NUMPAD_7	65429
+# define NUMPAD_8	65431
+# define NUMPAD_9	65434
+
+# define NUMKEY_1	49
+# define NUMKEY_2	50
+# define NUMKEY_3	51
+# define NUMKEY_4	52
+# define NUMKEY_5	53
+# define NUMKEY_6	54
+# define NUMKEY_7	55
+# define NUMKEY_8	56
+# define NUMKEY_9	57
+
+# define KEY_PLUS	61
+# define KEY_MINUS	45
+# define KEY_SPACE	32
+
+# define KEY_A		97
+# define KEY_C		99
+# define KEY_D		100
+# define KEY_E		101
+# define KEY_I		105
+# define KEY_L		108
+# define KEY_O		111
+# define KEY_P		112
+# define KEY_Q		113
+# define KEY_R		114
+# define KEY_S		115
+# define KEY_W		119
+# define KEY_X		120
+# define KEY_Z		122
+# define KEY_ESC	65307
+
+#endif
