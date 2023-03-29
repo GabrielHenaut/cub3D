fd7e88633d57fad8a97bc34412e480c7cae48574
GabrielHenaut <gabriel.henaut.peixoto@outlook.com>
2023-03-28 22:46:33 -0300

    fixed the map walls validation

--- src/validate_map.c
+++ src/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: harndt <harndt@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 21:13:27 by ghenaut-          #+#    #+#             */
-/*   Updated: 2023/03/23 16:43:49 by harndt           ###   ########.fr       */
+/*   Updated: 2023/03/28 22:43:50 by ghenaut-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,20 +34,56 @@ static int	is_letter(char c)
  * @param j Row array index.
  * @return int If outside the map 1, else 0.
  */
-static int	check_inside_map(t_cubed *data, int inside_map, int i, int j)
+static int	check_inside_map_line(t_cubed *data, int *inside_map, int i, int j)
 {
-	if (data->map.map[i][j] == ' ' && inside_map == TRUE)
+	if (data->map.map[i][j] == ' ' && *inside_map == TRUE)
 		if (data->map.map[i][j - 1] != '1')
 			return (1);
-	if (data->map.map[i][j] == ' ' && inside_map == FALSE)
+	if (data->map.map[i][j] == ' ' && *inside_map == FALSE)
 		if (data->map.map[i][j + 1] != '1' && \
 			data->map.map[i][j + 1] != ' ' && \
 			data->map.map[i][j + 1] != '\n')
 			return (1);
-	if (data->map.map[i][j] == '1' && inside_map == FALSE)
-		inside_map = TRUE;
+	if (data->map.map[i][j] == '1' && *inside_map == FALSE)
+		*inside_map = TRUE;
 	if (data->map.map[i][j] == ' ')
-		inside_map = FALSE;
+		*inside_map = FALSE;
+	if (data->map.map[i][j] == '0' && *inside_map == FALSE)
+		return (1);
+	if (data->map.map[i][j] == '0' && *inside_map == TRUE &&
+			j == (int)data->map.width - 2)
+			return (1);
+	return (0);
+}
+
+/**
+ * @brief Check the values inside the map.
+ * 
+ * @param data Address to the program struct.
+ * @param inside_map Int to tell if is inside or not in the map.
+ * @param i Column array index.
+ * @param j Row array index.
+ * @return int If outside the map 1, else 0.
+ */
+static int	check_inside_map_col(t_cubed *data, int *inside_map, int i, int j)
+{
+	if (data->map.map[i][j] == ' ' && *inside_map == TRUE)
+		if (data->map.map[i - 1][j] != '1')
+			return (1);
+	if (data->map.map[i][j] == ' ' && *inside_map == FALSE)
+		if (data->map.map[i + 1][j] != '1' && \
+			data->map.map[i + 1][j] != ' ' && \
+			data->map.map[i + 1][j] != '\n')
+			return (1);
+	if (data->map.map[i][j] == '1' && *inside_map == FALSE)
+		*inside_map = TRUE;
+	if (data->map.map[i][j] == ' ')
+		*inside_map = FALSE;
+	if (data->map.map[i][j] == '0' && *inside_map == FALSE)
+		return (1);
+	if (data->map.map[i][j] == '0' && *inside_map == TRUE &&
+			i == (int)data->map.height - 1)
+			return (1);
 	return (0);
 }
 
@@ -72,13 +108,22 @@ int	validate_map(t_cubed *data)
 		j = -1;
 		while (++j < data->map.width)
 		{
-			if (check_inside_map(data, inside_map, i, j) == 1)
+			if (check_inside_map_line(data, &inside_map, i, j) == 1)
 				return (1);
 			if (is_letter(data->map.map[i][j]) && counter > 0)
 				return (1);
 			if (is_letter(data->map.map[i][j]))
 				counter++;
 		}
+		inside_map = FALSE;
+	}
+	i = -1;
+	while (++i < data->map.width)
+	{
+		j = -1;
+		while (++j < data->map.height)
+			if (check_inside_map_col(data, &inside_map, j, i) == 1)
+				return (1);
 		inside_map = FALSE;
 	}
 	return (0);
